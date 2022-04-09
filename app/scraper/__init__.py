cc1c13fccc1a28172db462ae4d3537afbe9a69b9
ErikBoesen <me@erikboesen.com>
2022-04-09 00:41:44 -0400

    Tweak scraper

--- app/scraper/__init__.py
+++ app/scraper/__init__.py
@@ -26,6 +26,7 @@ from bs4 import BeautifulSoup, NavigableString
 yaleconnect_cookie = 'dtCookie=v_4_srv_6_sn_345D8D62EC5CF3008662D205540C82C2_perc_100000_ol_0_mul_1_app-3Aea7c4b59f27d43eb_1_app-3A1e70f254d6d57550_1; CG.SessionID=f44rgj4jlvc3psroe2d3yw4b-mwzfzfLk9IBpvv9mSae%2fJAizOr4%3d; cg_uid=1651934-bYlm4ZnwgHOMGvQdCu66jOzdvVGwKqHcx2oTVTJY8no='
 
 DEBUG = True
+ROOT = 'https://yaleconnect.yale.edu'
 
 def get_soup(url):
     r = requests.get(
@@ -35,7 +36,7 @@ def get_soup(url):
     return BeautifulSoup(r.text, 'html5lib')
 
 print('Reading groups list.')
-groups_soup = get_soup('https://yaleconnect.yale.edu/club_signup' if DEBUG else 'https://yaleconnect.yale.edu/club_signup?view=all').find('div', {'class': 'content-cont'})
+groups_soup = get_soup(ROOT + ('/club_signup' if DEBUG else '/club_signup?view=all')).find('div', {'class': 'content-cont'})
 rows = groups_soup.find('ul', {'class': 'list-group'}).find_all('li', {'class': 'list-group-item'})
 # Remove header
 rows.pop(0)
@@ -46,7 +47,7 @@ for row in rows:
     url = a['href']
     name = a.text.strip()
     groups.append({
-        'id': int(url.replace('https://yaleconnect.yale.edu/student_community?club_id=', '')),
+        'id': int(url.replace(ROOT + '/student_community?club_id=', '')),
         'name': name,
     })
 
@@ -54,7 +55,7 @@ print(groups)
 
 for i in range(len(groups)):
     group_id = groups[i]['id']
-    about_soup = get_soup(f'https://yaleconnect.yale.edu/ajax_group_page_about?ax=1&club_id={group_id}').find('div', {'class': 'card-block'})
+    about_soup = get_soup(f'{ROOT}/ajax_group_page_about?ax=1&club_id={group_id}').find('div', {'class': 'card-block'})
     current_header = None
     current_contact_property = None
     for child in about_soup.children:
@@ -66,7 +67,8 @@ for i in range(len(groups)):
                     text = child.text.strip()
                     if not text:
                         continue
-                    prop, value = text.split(': ')
+                    print(text)
+                    prop, value = text.split(': ', 1)
                     prop = prop.lower().replace(' ', '_')
                     prop = {
                         'group_type': 'type',
@@ -92,10 +94,8 @@ for i in range(len(groups)):
                     groups[i]['mission'] += text
             elif current_header == 'CONSTITUTION':
                 if child.name == 'p':
-                    groups[i]['constitution'] = 'https://yaleconnect.yale.edu' + child.find('a')['href']
+                    groups[i]['constitution'] = ROOT + child.find('a')['href']
             elif current_header == 'CONTACT INFO':
-                print(child)
-                print(child.name)
                 if child.name == 'span' and 'class' in child and 'mdi' in child['class']:
                     current_contact_property = child['class'][1].replace('mdi-', '')
                 else:
@@ -109,11 +109,21 @@ for i in range(len(groups)):
                             groups[i]['address'] = text
                         else:
                             print(f'Saw unrecognized contact property {current_contact_property} with value {text}.')
-
             elif current_header == 'OFFICERS':
-                pass
+                if 'officers' not in groups[i]:
+                    groups[i]['officers'] = []
+                if child.name == 'img':
+                    officer = {}
+                    officer['name'] = child['alt'].replace('Profile image for ', '')
+                    ajax_path = child['onclick'].split('\'')[1]
+                    profile_soup = get_soup(ROOT + ajax_path)
+                    email_li = profile_soup.find('li', {'class': 'mdi-email'})
+                    if email_li:
+                        officer['email'] = email_li.find('a')['href'].replace('mailto:', '')
+                    groups[i]['officers'].append(officer)
             elif current_header is None:
                 pass
             else:
                 print(f'Encountered unknown About header {current_header}.')
-    break
+
+print(groups)
