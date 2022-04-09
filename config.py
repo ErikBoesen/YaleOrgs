a27d72f7233b3808d7bde1c44f27bf98c6eefa15
ErikBoesen <me@erikboesen.com>
2022-04-08 20:46:24 -0400

    Add config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    PAGE_SIZE = 20
+
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Override this in production')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                             'sqlite:///' + os.path.join(basedir, 'app.db')).replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_URL = CELERY_BROKER_URL = CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+
+    CAS_SERVER = 'https://secure.its.yale.edu'
+    CAS_AFTER_LOGIN = 'index'
+    CAS_LOGIN_ROUTE = '/cas/login'
