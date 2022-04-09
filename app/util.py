8fc51e016ea9f10451c4f5155d58b7ae69e0a524
ErikBoesen <me@erikboesen.com>
2022-04-09 01:29:03 -0400

    Allow expanding models

--- app/util.py
+++ app/util.py
@@ -25,6 +25,11 @@ def fail(message, code=400):
 
 
 class ModelEncoder(json.JSONEncoder):
+    def val_to_string(self, val):
+        if type(val) == datetime.date:
+            return val.strftime(DATE_FMT)
+        return val
+
     def default(self, obj):
         if isinstance(obj.__class__, DeclarativeMeta):
             # go through each field in this SQLalchemy class
@@ -32,12 +37,14 @@ class ModelEncoder(json.JSONEncoder):
             for field in obj.__class__.__serializable__:
                 val = obj.__getattribute__(field)
 
-                # is this field another SQLalchemy object, or a list of SQLalchemy objects, or a function?
-                if isinstance(val.__class__, DeclarativeMeta) or (isinstance(val, list) and len(val) > 0 and isinstance(val[0].__class__, DeclarativeMeta)) or callable(val):
-                    # Skip this field
-                    continue
-
-                fields[field] = val
+                # is this field another SQLalchemy object, or a list of SQLalchemy objects?
+                if isinstance(val.__class__, DeclarativeMeta) or (isinstance(val, list) and len(val) > 0 and isinstance(val[0].__class__, DeclarativeMeta)):
+                    # unless we're expanding this field, stop here
+                    if field not in obj.__class__._to_expand:
+                        # not expanding this field: set it to None and continue
+                        fields[field] = None
+                        continue
+                fields[field] = self.val_to_string(val)
             # a json-encodable dict
             return fields
 
