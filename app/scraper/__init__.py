453024fe3307a659a037872387914a64da564685
ErikBoesen <me@erikboesen.com>
2022-04-09 02:15:38 -0400

    Remove null strings in scraper

--- app/scraper/__init__.py
+++ app/scraper/__init__.py
@@ -121,6 +121,7 @@ def scrape(yaleconnect_cookie):
         organization = Organization(**organization_dict)
         db.session.add(organization)
         organization.officers[:] = []
+        organization_dict = {prop: value for prop, value in organization_dict.items() if value}
         for officer in officers:
             person = Person.query.get(officer['id'])
             if not person:
