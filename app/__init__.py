666f72dae0a2627fbbbfc2f28cf84cb8c016b2bf
ErikBoesen <me@erikboesen.com>
2022-04-08 21:00:58 -0400

    Add __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config import Config
+from celery import Celery
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cas import CAS
+from flask_redis import FlaskRedis
+from elasticsearch import Elasticsearch
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+cors = CORS(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+cas = CAS(app)
+redis = FlaskRedis(app)
+elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    if app.config['ELASTICSEARCH_URL'] else None
+
+from app import routes, errors, api
+app.register_blueprint(api.api_bp, url_prefix='/api')
